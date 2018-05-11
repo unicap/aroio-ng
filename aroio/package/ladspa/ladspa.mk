a2af90e78598eeb9454f15410b5315e125487fe8
Nicola <nb@komeda-berlin.de>
2018-05-11 13:33:01 +0200

    Added package ladspa (not yet compiling, though..)

---
+++ aroio/package/ladspa/ladspa.mk
@@ -0,0 +1,31 @@
+#############################################################
+#
+# lasdpa
+#
+#############################################################
+LADSPA_VERSION = 692b4001120aea16385d74ce86b0865c6ce7fdf5
+LADSPA_SOURCE = ladspa-$(LADSPA_VERSION).tar.gz
+LADSPA_SITE = $(call github,swh,ladspa,$(LADSPA_VERSION))
+LADSPA_AUTORECONF = YES
+LADSPA_AUTORECONF_OPTS = -i
+LADSPA_GETTEXTIZE = YES
+LADSPA_INSTALL_STAGING = NO
+
+
+LADSPA_DEPENDENCIES = host-gettext $(TARGET_NLS_DEPENDENCIES)
+#LADSPA_CONF_OPTS = --prefix=/usr --disable-nls --disable-rpath
+
+LADSPA_SECTION = audio
+LADSPA_DESCRIPTION = Steve Harris LADSPA plugins
+
+
+define LADSPA_PRE_CONFIGURE_FIXUP
+	mkdir -p $(@D)/m4
+#	touch $(@D)/ABOUT-NLS
+#	touch $(@D)/config.rpath
+endef
+
+
+#LADSPA_PRE_CONFIGURE_HOOKS += LADSPA_PRE_CONFIGURE_FIXUP
+
+$(eval $(autotools-package))
