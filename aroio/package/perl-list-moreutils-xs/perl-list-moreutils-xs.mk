b2ca3a7b645b2d20c899caaff9e40132f7922a10
arne <arne.caspari@gmail.com>
2018-05-29 16:56:04 +0200

    Add perl-list-moreutils-xs

---
+++ aroio/package/perl-list-moreutils-xs/perl-list-moreutils-xs.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# perl-list-moreutils-xs
+#
+################################################################################
+
+PERL_LIST_MOREUTILS_XS_VERSION = 0.428
+PERL_LIST_MOREUTILS_XS_SOURCE = List-MoreUtils-XS-$(PERL_LIST_MOREUTILS_XS_VERSION).tar.gz
+PERL_LIST_MOREUTILS_XS_SITE = $(BR2_CPAN_MIRROR)/authors/id/R/RE/REHSACK
+PERL_LIST_MOREUTILS_XS_DEPENDENCIES = host-perl-config-autoconf host-perl-inc-latest
+PERL_LIST_MOREUTILS_XS_LICENSE = Apache-2.0
+PERL_LIST_MOREUTILS_XS_LICENSE_FILES = ARTISTIC-1.0 LICENSE
+
+$(eval $(perl-package))
