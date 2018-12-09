209fd992f0195f23cdf7bb59dfcc26c223e86a25
sn <h3lmchen@googlemail.com>
2018-12-09 14:22:34 -0500

    fixed compilation erros, some minor cleanups

--- aroio/package/jack-connect-persistent/jack-connect-persistent.mk
+++ aroio/package/jack-connect-persistent/jack-connect-persistent.mk
@@ -5,7 +5,6 @@
 ################################################################################
 
 JACK_CONNECT_PERSISTENT_VERSION = master
-#JACK_CONNECT_PERSISTENT_SITE = git@github.com:worosom/jack_connect_persistent.git
 JACK_CONNECT_PERSISTENT_SOURCE = jack_connect_persistent-$(JACK_CONNECT_PERSISTENT_VERSION).tar.gz
 JACK_CONNECT_PERSISTENT_SITE = $(call github,worosom,jack_connect_persistent,$(JACK_CONNECT_PERSISTENT_VERSION))
 
@@ -13,17 +12,19 @@ JACK_CONNECT_PERSISTENT_LICENSE = GPLv2
 JACK_CONNECT_PERSISTENT_LICENSE_FILES = GPL-2.0
 JACK_CONNECT_PERSISTENT_INSTALL_TARGET = YES
 JACK_CONNECT_PERSISTENT_DEPENDENCIES = jack2
+JACK_CONNECT_PERSISTENT_MAKE_OPTS = -DJACK
 
 define JACK_CONNECT_PERSISTENT_BUILD_CMDS
-        $(MAKE) CC="$(TARGET_CPP)" LD="$(TARGET_LD)" -C $(@D) all
+        $(TARGET_CONFIGURE_OPTS) $(MAKE) CC="$(TARGET_CXX)"\
+		OPTS="$(JACK_CONNECT_PERSISTENT_MAKE_OPTS)" -C $(@D) all
 endef
 
 define JACK_CONNECT_PERSISTENT_INSTALL_TARGET_CMDS
-        $(INSTALL) -D -m 0755 $(@D)/jack_persistent_client $(TARGET_DIR)/bin
+        $(INSTALL) -D -m 0755 $(@D)/jack_persistent_client $(TARGET_DIR)/usr/bin
 endef
 
 define JACK_CONNECT_PERSISTENT_PERMISSIONS
-       /bin/jack_persistent_client f 4755 0 0 - - - - - 
+       /usr/bin/jack_persistent_client f 4755 0 0 - - - - - 
 endef
 
 $(eval $(generic-package))
