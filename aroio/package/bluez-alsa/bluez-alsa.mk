be698966cbc3f86ee21927e6cb9f927e226bbf05
mr-berndt <nb@komeda-berlin.de>
2019-04-29 13:05:40 +0200

    Bluez-Alsa: Actually changed nothing, but touched the file.

--- aroio/package/bluez-alsa/bluez-alsa.mk
+++ aroio/package/bluez-alsa/bluez-alsa.mk
@@ -5,13 +5,15 @@
 ################################################################################
 
 BLUEZ_ALSA_VERSION = 9045edb436ea755f395a2e09e4525b5defad286a
+#BLUEZ_ALSA_VERSION = 8505bab
+#BLUEZ_ALSA_VERSION = 11eb11d
 BLUEZ_ALSA_SOURCE = bluez-alsa-$(BLUEZ_ALSA_VERSION).tar.gz
 BLUEZ_ALSA_SITE = $(call github,Arkq,bluez-alsa,$(BLUEZ_ALSA_VERSION))
 BLUEZ_ALSA_AUTORECONF = YES
 BLUEZ_ALSA_INSTALL_STAGING = NO
 #BLUEZ_ALSA_INSTALL_TARGET = YES
 
-BLUEZ_ALSA_CONF_OPTS = --prefix=/usr --disable-pcm-test --with-alsaplugindir=/usr/lib/alsa-lib --with-alsadatadir=/usr/share/alsa 
+BLUEZ_ALSA_CONF_OPTS = --disable-payloadcheck --enable-aac --prefix=/usr --disable-pcm-test --with-alsaplugindir=/usr/lib/alsa-lib --with-alsadatadir=/usr/share/alsa 
 
 BLUEZ_ALSA_DEPENDENCIES = alsa-lib sbc fdk-aac
 
