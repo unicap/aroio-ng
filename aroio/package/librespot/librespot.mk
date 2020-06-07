d12169d82a41613ead65cb4c8103fd1d19a3a141
Nicola Berndt <nb@komeda-berlin.de>
2020-06-07 15:34:57 +0200

    Packages: Added librespot

---
+++ aroio/package/librespot/librespot.mk
@@ -0,0 +1,34 @@
+################################################################################
+#
+# librespot
+#
+################################################################################
+
+LIBRESPOT_VERSION = master
+LIBRESPOT_SITE = $(call github,librespot-org,librespot,$(LIBRESPOT_VERSION))
+LIBRESPOT_LICENSE = GPL-3.0+
+LIBRESPOT_LICENSE_FILES = COPYING
+
+LIBRESPOT_DEPENDENCIES = host-cargo alsa-lib openssl
+LIBRESPOT_CARGO_ENV = CARGO_HOME=$(HOST_DIR)/share/cargo
+LIBRESPOT_CARGO_MODE = $(if $(BR2_ENABLE_DEBUG),debug,release)
+
+LIBRESPOT_BIN_DIR = target/$(RUSTC_TARGET_NAME)/$(LIBRESPOT_CARGO_MODE)
+
+LIBRESPOT_CARGO_OPTS = \
+  --$(LIBRESPOT_CARGO_MODE) \
+    --target=$(RUSTC_TARGET_NAME) \
+    --manifest-path=$(@D)/Cargo.toml \
+    --no-default-features --features alsa-backend,jackaudio-backend,rodio-backend
+
+define LIBRESPOT_BUILD_CMDS
+    $(TARGET_MAKE_ENV) $(LIBRESPOT_CARGO_ENV) \
+            cargo build $(LIBRESPOT_CARGO_OPTS)
+endef
+
+define LIBRESPOT_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/$(LIBRESPOT_BIN_DIR)/LIBRESPOT \
+            $(TARGET_DIR)/usr/bin/LIBRESPOT
+endef
+
+$(eval $(generic-package))
