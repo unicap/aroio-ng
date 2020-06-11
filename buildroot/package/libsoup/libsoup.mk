7cb7326720cc943fbbc737ae9bea7232248cff6e
Arne Caspari <arne@unicap-imaging.org>
2020-06-11 13:52:42 +0200

    Disable introspection for libsoup

--- buildroot/package/libsoup/libsoup.mk
+++ buildroot/package/libsoup/libsoup.mk
@@ -12,7 +12,7 @@ LIBSOUP_LICENSE = LGPL-2.0+
 LIBSOUP_LICENSE_FILES = COPYING
 LIBSOUP_INSTALL_STAGING = YES
 LIBSOUP_CONF_ENV = ac_cv_path_GLIB_GENMARSHAL=$(LIBGLIB2_HOST_BINARY) PERL=$(HOST_DIR)/bin/perl
-LIBSOUP_CONF_OPTS = --disable-glibtest --enable-vala=no --with-gssapi=no
+LIBSOUP_CONF_OPTS = --disable-glibtest --enable-vala=no --with-gssapi=no --disable-introspection
 LIBSOUP_DEPENDENCIES = host-pkgconf host-libglib2 \
 	libglib2 libxml2 sqlite host-intltool
 
