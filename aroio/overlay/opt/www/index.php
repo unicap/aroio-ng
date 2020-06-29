edc17798a6d4bc65be080eb00e6d633f3804cece
Nicola Berndt <nb@komeda-berlin.de>
2020-06-29 11:20:27 +0200

    Changed default jack buffer values for 96kHz, since with newer alsa versions 8192 doesn not work anymore.

--- aroio/overlay/opt/www/index.php
+++ aroio/overlay/opt/www/index.php
@@ -81,7 +81,7 @@
         break;
 
         case 96000:
-          $_POST[JACKBUFFER]=8192;
+          $_POST[JACKBUFFER]=4096;
           $_POST[JACKPERIOD]=3;
           $_POST[SQUEEZE_ALSABUFFER]=8192;
           $_POST[SQUEEZE_ALSAPERIOD]=4;
@@ -95,7 +95,7 @@
         break;
 
         case 192000:
-          $_POST[JACKBUFFER]=8192;
+          $_POST[JACKBUFFER]=4096;
           $_POST[JACKPERIOD]=3;
           $_POST[SQUEEZE_ALSABUFFER]=4096;
           $_POST[SQUEEZE_ALSAPERIOD]=2;
