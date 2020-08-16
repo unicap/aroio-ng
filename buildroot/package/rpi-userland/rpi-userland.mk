315a17f15bdd624cb92c945ff71ed6fd930bdc4e
Nicola Berndt <nb@komeda-berlin.de>
2020-08-16 12:29:20 +0200

    System: Updated firmware and userland versions.

--- buildroot/package/rpi-userland/rpi-userland.mk
+++ buildroot/package/rpi-userland/rpi-userland.mk
@@ -4,7 +4,9 @@
 #
 ################################################################################
 
-RPI_USERLAND_VERSION = 06bc6daa02137ca72b7a2104afad81e82a44de17
+RPI_USERLAND_VERSION = 3e59217bd93b8024fb8fc1c6530b00cbae64bc73
+#188d3bfe4a0ac36b119a2cee35a6be8d0c68e09e
+#06bc6daa02137ca72b7a2104afad81e82a44de17
 RPI_USERLAND_SITE = $(call github,raspberrypi,userland,$(RPI_USERLAND_VERSION))
 RPI_USERLAND_LICENSE = BSD-3-Clause
 RPI_USERLAND_LICENSE_FILES = LICENCE
