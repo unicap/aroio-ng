86bf89f2b2ecfd67432b82b73ee85b724ae06119
Melf Stöcken <melf11>
2020-11-10 03:56:16 -0800

    merged upstream

--- aroio/package/webinterface/webinterface.mk
+++ aroio/package/webinterface/webinterface.mk
@@ -4,28 +4,17 @@
 #
 ################################################################################
 
-<<<<<<< HEAD
 WEBINTERFACE_VERSION = 2.0.7
-=======
-WEBINTERFACE_VERSION = b62ae0d5439e8dd72bffe34817cab298c1e8bcb4
->>>>>>> upstream/aroio-ng-2020.02
 WEBINTERFACE_GIT_SUBMODULES = YES
 WEBINTERFACE_SITE_METHOD = git
 WEBINTERFACE_SITE = git://github.com/aroio/webinterface
 WEBINTERFACE_LICENSE = MIT
 WEBINTERFACE_DEPENDENCIES = python3 python-uvicorn python-fastapi
 
-<<<<<<< HEAD
 #define WEBINTERFACE_BUILD_CMDS
 #	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) \
 #		-C $(@D) all
 #endef
-=======
-define WEBINTERFACE_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) \
-		-C $(@D) all
-endef
->>>>>>> upstream/aroio-ng-2020.02
 
 define WEBINTERFACE_INSTALL_TARGET_CMDS
 	rm -rf $(TARGET_DIR)/opt/www
