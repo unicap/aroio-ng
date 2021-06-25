1c977fe6c5e46696c3a8d762c84d7e77390de76d
mrberndt <nb@komeda-berlin.de>
2021-06-25 20:31:03 +0200

    Changed download location of brutefir package.

--- aroio/package/brutefir/brutefir.mk
+++ aroio/package/brutefir/brutefir.mk
@@ -6,15 +6,16 @@
 
 BRUTEFIR_VERSION = 1.0m
 BRUTEFIR_SOURCE = brutefir-$(BRUTEFIR_VERSION).tar.gz
-BRUTEFIR_SITE = http://www.ludd.luth.se/~torger/files
+#BRUTEFIR_SITE = http://www.ludd.luth.se/~torger/files
+BRUTEFIR_SITE = https://torger.se/anders/files
 BRUTEFIR_LICENSE = GPLv2
 BRUTEFIR_LICENSE_FILES = GPL-2.0
 BRUTEFIR_INSTALL_STAGING = NO
-BRUTEFIR_DEPENDENCIES = fftw-double fftw3f jack2 host-flex
+BRUTEFIR_DEPENDENCIES = fftw fftw3f jack2
 
 ifeq ($(KERNEL_ARCH),arm)
 define BRUTEFIR_BUILD_CMDS
-	$(MAKE) UNAME_M="armv7" LIBPATHS="-L$(STAGING_DIR)/usr/lib" INCLUDE="-I$(STAGING_DIR)/usr/include" FLEX="$(HOST_DIR)/bin/flex" CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) all
+	$(MAKE) UNAME_M="armv7" LIBPATHS="-L$(STAGING_DIR)/usr/lib" INCLUDE="-I$(STAGING_DIR)/usr/include" CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) all
 endef
 else
 define BRUTEFIR_BUILD_CMDS
